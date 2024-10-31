325b3c24047391bf827a2c3c0901a0513150f77b
李苍龙 <176152949@qq.com>
2024-10-31 14:11:13 +0800

    已修改        index.html 已修改        scripts.js 已修改        styles.css 已添加        image/1_thumb.jpg 已添加        image/2_thumb.jpg 已添加        image/3_thumb.jpg

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let currentIndex = 0;  
-  
+let autoPlayInterval = null;  
+const autoPlaySpeed = 2000; // 自动播放速度，单位为毫秒（这里设置为3秒）
+
 function showSlide(index) {  
     const slides = document.querySelectorAll('.carousel-slide');  
     if (index >= slides.length) {  
@@ -20,6 +22,36 @@ function nextSlide() {
 function prevSlide() {  
     showSlide(currentIndex - 1);  
 }  
+
+function startAutoPlay() {  
+    if (autoPlayInterval == null) {  
+        autoPlayInterval = setInterval(nextSlide, autoPlaySpeed);  
+    }  
+}  
   
+function pauseAutoPlay() {  
+    if (autoPlayInterval != null) {  
+        clearInterval(autoPlayInterval);  
+        autoPlayInterval = null;  
+    }  
+}  
+ 
+// 监听鼠标进入和离开轮播图容器，以暂停和恢复自动轮播  
+const carouselContainer = document.querySelector('.carousel-container');  
+carouselContainer.addEventListener('mouseenter', pauseAutoPlay);  
+carouselContainer.addEventListener('mouseleave', startAutoPlay);  
+  
+// 监听缩略图的点击事件，同样暂停自动轮播  
+const thumbnails = document.querySelectorAll('.thumbnail');  
+thumbnails.forEach(thumbnail => {  
+    // thumbnail.addEventListener('mouseenter', pauseAutoPlay); 
+    thumbnail.addEventListener('mouseleave', startAutoPlay); 
+	thumbnail.addEventListener('mouseenter', (event) => {  
+        const index = Array.prototype.indexOf.call(thumbnails, event.target);  
+        showSlide(index);
+		pauseAutoPlay();
+    });  
+});  
+
 // 自动播放功能（可选）  
-setInterval(nextSlide, 3000);
+startAutoPlay();
